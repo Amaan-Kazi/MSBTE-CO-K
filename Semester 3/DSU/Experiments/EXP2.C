e6dfe42d1a34007127e44fe4518c63ec77942453
Amaan Kazi <85413461+Amaan-Kazi@users.noreply.github.com>
2024-08-16 10:22:08 +0530

    DSU Experiment 2

---
+++ Semester 3/DSU/Experiments/EXP2.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main ()
+{
+	int arr[100], n, search, i, flag = 0;
+	clrscr();
+
+	printf("Number of elements: ");
+	scanf("%d", &n);
+
+	printf("Enter array \n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d. ", i);
+		scanf("%d", &arr[i]);
+	}
+
+	printf("Search Element: ");
+	scanf("%d", &search);
+
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i] == search)
+		{
+			flag = 1;
+			printf("Search element %d found on index %d", search, i);
+			break;
+		}
+	}
+
+	if (flag == 0)
+	{
+		printf("Not found");
+	}
+
+	getch();
+}
