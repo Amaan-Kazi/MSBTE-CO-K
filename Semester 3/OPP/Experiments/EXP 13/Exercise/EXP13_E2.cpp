06a3616e622a5e429cb45a51aabd709030b21a09
Amaan Kazi <85413461+Amaan-Kazi@users.noreply.github.com>
2024-09-06 15:58:28 +0530

    OPP EXP13

---
+++ Semester 3/OPP/Experiments/EXP 13/Exercise/EXP13_E2.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+using namespace std;
+
+class Staff {
+public:
+    int code;
+    string name;
+
+    void input() {
+        cout << "Enter staff code: ";
+        cin >> code;
+        cin.ignore(); // Ignore newline character left in the buffer after integer input
+        cout << "Enter staff name: ";
+        getline(cin, name);
+    }
+
+    void display() const {
+        cout << "Code: " << code << endl;
+        cout << "Name: " << name << endl;
+    }
+};
+
+class Teacher : public Staff {
+public:
+    string subject;
+    string publication;
+
+    void input() {
+        Staff::input();
+        cout << "Enter subject: ";
+        getline(cin, subject);
+        cout << "Enter publication: ";
+        getline(cin, publication);
+    }
+
+    void display() const {
+        Staff::display();
+        cout << "Subject: " << subject << endl;
+        cout << "Publication: " << publication << endl;
+    }
+};
+
+class Officer : public Staff {
+public:
+    int grade;
+
+    void input() {
+        Staff::input();
+        cout << "Enter grade: ";
+        cin >> grade;
+        cin.ignore(); // Ignore newline character left in the buffer after integer input
+    }
+
+    void display() const {
+        Staff::display();
+        cout << "Grade: " << grade << endl;
+    }
+};
+
+class Typist : public Staff {
+public:
+    float speed;
+
+    void input() {
+        Staff::input();
+        cout << "Enter typing speed: ";
+        cin >> speed;
+        cin.ignore(); // Ignore newline character left in the buffer after float input
+    }
+
+    void display() const {
+        Staff::display();
+        cout << "Typing Speed: " << speed << " words per minute" << endl;
+    }
+};
+
+class Regular : public Typist {
+public:
+    int wages2;
+
+    void input() {
+        Typist::input();
+        cout << "Enter regular wages: ";
+        cin >> wages2;
+        cin.ignore(); // Ignore newline character left in the buffer after integer input
+    }
+
+    void display() const {
+        Typist::display();
+        cout << "Regular Wages: " << wages2 << endl;
+    }
+};
+
+class Casual : public Typist {
+public:
+    float wages;
+
+    void input() {
+        Typist::input();
+        cout << "Enter casual wages: ";
+        cin >> wages;
+        cin.ignore(); // Ignore newline character left in the buffer after float input
+    }
+
+    void display() const {
+        Typist::display();
+        cout << "Casual Wages: " << wages << endl;
+    }
+};
+
+int main() {
+    Teacher t1;
+    Officer o1;
+    Regular r1;
+    Casual c1;
+
+    cout << "Enter details for Teacher:" << endl;
+    t1.input();
+    cout << "\nDetails of Teacher:" << endl;
+    t1.display();
+
+    cout << "\nEnter details for Officer:" << endl;
+    o1.input();
+    cout << "\nDetails of Officer:" << endl;
+    o1.display();
+
+    cout << "\nEnter details for Regular Typist:" << endl;
+    r1.input();
+    cout << "\nDetails of Regular Typist:" << endl;
+    r1.display();
+
+    cout << "\nEnter details for Casual Typist:" << endl;
+    c1.input();
+    cout << "\nDetails of Casual Typist:" << endl;
+    c1.display();
+
+    return 0;
+}
+
