f9f200e20c08c54227c8921fd88cbada823656b5
Amaan Kazi <85413461+Amaan-Kazi@users.noreply.github.com>
2024-09-20 15:59:53 +0530

    OPP EXP16

---
+++ Semester 3/OPP/Experiments/EXP 16/Practical Related Questions/EXP16_P2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+// Write a C++ program to declare a class "Box" having data members height,
+//width and breadth. Accept this information for one object using pointer to
+//that object . Display the area and volume of that object.
---
+++ Semester 3/OPP/Experiments/EXP 16/Practical Related Questions/EXP16_P3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Birthday {
+    
+    public:
+    int day;
+    int month;
+    int year;
+    
+    void display()
+    {
+        cout << day << "/" << month << year << endl;
+    }
+};
