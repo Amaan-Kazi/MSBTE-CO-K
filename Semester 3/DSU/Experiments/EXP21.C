b7e7e209370fa2b9efe7e71d1b905e822fea3651
Amaan Kazi <85413461+Amaan-Kazi@users.noreply.github.com>
2024-10-11 11:11:22 +0530

    DSU EXP21

--- Semester 3/DSU/Experiments/EXP21.C
+++ Semester 3/DSU/Experiments/EXP21.C
@@ -1,38 +1,74 @@
 #include <stdio.h>
 #include <conio.h>
+#define MAX 20  // Maximum size of the queue
 
-void main ()
-{
-	int queue[100], f = 0, r = -1, val, input = -1;
-	clrscr();
+int queue[MAX];
+int front = -1, rear = -1;
 
-	while (input != 0)
-	{
-		printf("0. Exit \n");
-		printf("1. Enqueue \n");
-		printf("2. Dequeue \n");
-		printf("3. Display \n");
-		printf("Input: ");
-		scanf("%d", &input);
+int main() {
+	int choice, value, i;
 
-		switch (input)
-		{
+	while (1) {
+		clrscr();
+		printf("Menu:\n");
+		printf("0: Exit\n");
+		printf("1: Enqueue\n");
+		printf("2: Dequeue\n");
+		printf("3: Display\n");
+		printf("Enter your choice: ");
+		scanf("%d", &choice);
+
+		switch (choice) {
 			case 0:
-				exit(1);
-				break;
-			case 1:
-				printf("\n Value: ");
-				scanf("%d", &val);
-			case 2:
+				printf("Exiting...\n");
+				return 0;
 
-			case 3:
+			case 1: // Enqueue
+				if (rear == MAX - 1)
+					printf("Queue is full!\n");
+				else {
+					if (front == -1)
+						front = 0;  // First element
+					printf("Enter value to enqueue: ");
+					scanf("%d", &value);
+					rear++;
+					queue[rear] = value;
+					printf("Inserted %d into the queue.\n", value);
+				}
+				getch();
+				break;
+
+			case 2: // Dequeue
+				if (front == -1 || front > rear)
+					printf("Queue is empty!\n");
+				else
+				{
+					value = queue[front];
+					front++;
+					printf("Deleted %d from the queue.\n", value);
+				}
+				getch();
+				break;
+
+			case 3: // Display
+				if (front == -1 || front > rear)
+					printf("Queue is empty!\n");
+				else
+				{
+					printf("Queue elements: ");
+					for (i = front; i <= rear; i++)
+					{
+						printf("%d ", queue[i]);
+					}
+					printf("\n");
+				}
+				getch();
+				break;
 
 			default:
-				printf("Invalid \n");
-				break;
-
+				printf("Invalid choice! Please try again.\n");
 		}
 	}
 
-	getch();
+	return 0;
 }
